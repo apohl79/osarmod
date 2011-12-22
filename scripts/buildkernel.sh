42f63d0adeced0023faac2e17c2088492baedefd
Andreas Pohl <apohl79@gmail.com>
2011-12-22 14:03:37 +0000

    o new version scheme o copy boot.img to romroot

--- scripts/buildkernel.sh
+++ scripts/buildkernel.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-VERSION_NAME=osar
+setkernelversion.sh
+
+VERSION_NAME=osarmod
+VERSION_NUM=`cat $HOME/android/VERSION_KERNEL`
 BOOTIMG_DIR=$HOME/android/kernel/bootimg
 BOOTIMG_OUT=$HOME/android/build
+ROMROOT_DIR=$HOME/android/romroot
 HELPER_DIR=$HOME/android/kernel/helper-files
 KERNEL_DIR=$HOME/android/kernel/osarmod-cm-kernel
 BUILD_DIR="$KERNEL_DIR/build"
 MODULES=("drivers/net/wireless/bcm4329/bcm4329.ko" "fs/cifs/cifs.ko" "fs/fuse/fuse.ko" "drivers/net/tun.ko")
-DATE=`date +%Y%m%d`
-FLASH_ZIP="$BOOTIMG_OUT/kernel-osar"
-INC=1
+FLASH_ZIP="$BOOTIMG_OUT/osarmod-cm7-kernel"
 
 build ()
 {
     local target=$1
-    echo "-$VERSION_NAME-$DATE-$INC" > localversion
-    #mv $KERNEL_DIR/drivers/video/samsung/logo_rgb24_wvga_portrait.h $KERNEL_DIR/drivers/video/samsung/logo_rgb24_wvga_portrait.h.org
-    #cp $HELPER_DIR/logo/logo_rgb24_wvga_portrait.h $KERNEL_DIR/drivers/video/samsung/
-    ./build.sh $target
-    rm -f localversion
-    #mv $KERNEL_DIR/drivers/video/samsung/logo_rgb24_wvga_portrait.h.org $KERNEL_DIR/drivers/video/samsung/logo_rgb24_wvga_portrait.h
+    export LOCALVERSION="-$VERSION_NAME-$VERSION_NUM"
+     ./build.sh $target
 }
 
 package ()
@@ -34,11 +32,13 @@ package ()
     echo "Copying modules for $target..."
     for module in "${MODULES[@]}" ; do
         cp "$target_dir/$module" $BOOTIMG_DIR/system/lib/modules
+        cp "$target_dir/$module" $ROMROOT_DIR/$target/system/lib/modules
     done
 
     echo "Creating CWM flashable zip for $target..."
     $ANDROID_BUILD_TOP/device/samsung/aries-common/mkshbootimg.py $BOOTIMG_DIR/boot.img "$target_dir"/arch/arm/boot/zImage $HELPER_DIR/ramdisk.img $HELPER_DIR/ramdisk-recovery.img
     cd $BOOTIMG_DIR
+    cp boot.img $ROMROOT_DIR/$target
     zip -q -r $BOOTIMG_OUT/tmpupdate.zip .
     signzip $BOOTIMG_OUT/tmpupdate.zip $FLASH_ZIP 
     rm $BOOTIMG_OUT/tmpupdate.zip
@@ -51,12 +51,7 @@ if [ -n "$1" ] ; then
     target=$1
 fi
 
-FLASH_ZIP_T="$FLASH_ZIP-$target-$DATE-$INC-signed.zip"
-while [ -e "$FLASH_ZIP_T" ]; do
-    let INC++
-    FLASH_ZIP_T="$FLASH_ZIP-$target-$DATE-$INC-signed.zip"
-done
-FLASH_ZIP=$FLASH_ZIP_T
+FLASH_ZIP="$FLASH_ZIP-$target-$VERSION_NUM-signed.zip"
 
 cd $KERNEL_DIR
 build $target
