c5eef1457ebfeb877c4ab8b5737034ec56e87dd3
Andreas Pohl <apohl79@gmail.com>
2012-07-26 11:31:25 +0200

    Added support for CM10 builds

--- scripts/buildall.sh
+++ scripts/buildall.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-DEVICE_COUNT=2
+case $OSARMOD_OS in
+    cm9)
+	DEVICE_START=1
+	DEVICE_END=2
+	;;
+    cm10)
+	DEVICE_START=3
+	DEVICE_END=4
+	;;
+esac
 
 if [ "$DEVBUILD" != "1" ]; then
     echo "CLEAN UP"
@@ -8,7 +17,7 @@ if [ "$DEVBUILD" != "1" ]; then
     make clean
 fi
 
-for d in $(seq 1 $DEVICE_COUNT); do
+for d in $(seq $DEVICE_START $DEVICE_END); do
     export OSARMOD_INIT=$d
     . $HOME/android/init.sh
     buildrom.sh
